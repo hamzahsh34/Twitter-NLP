598de06ddefb0668b370f26560bfda1110411b73
hamzahs34 <hamzah_shahid@hotmail.com>
2019-08-28 01:09:55 -0400

    Use Twitter API to scrape Tweet data

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -7,16 +7,24 @@
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let swifter = Swifter(consumerKey: "47cSlx0Ui5kXKRP0H6yYkRjuv", consumerSecret: "zrgHiQmRhpZ0C6zde9Fpll4I21VROtFn2yuO68dQdLhTfc4xZ8")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //ss
+        
+        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
+            print(results)
+        }) { (error) in
+            print(error)
+        }
     }
 
     @IBAction func predictPressed(_ sender: Any) {
