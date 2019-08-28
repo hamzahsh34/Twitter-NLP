1ddc29281d241fa25c52262e0d5b589f4f12a432
hamzahs34 <hamzah_shahid@hotmail.com>
2019-08-28 01:26:38 -0400

    Add pod SwiftyJSON to parse tweets

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import SwifteriOS
+import CoreML
+import SwiftyJSON
 
 class ViewController: UIViewController {
     
@@ -15,13 +17,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
     
+    let sentimentClassifier = TweetSentimentClassifier()
+    
     let swifter = Swifter(consumerKey: "47cSlx0Ui5kXKRP0H6yYkRjuv", consumerSecret: "zrgHiQmRhpZ0C6zde9Fpll4I21VROtFn2yuO68dQdLhTfc4xZ8")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        let prediction = try! sentimentClassifier.prediction(text: "@Apple")
+        
         swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
-            print(results)
+            
+            var tweets = [String]()
+            
+            for i in 0..<100 {
+                // SwiftyJSON
+                if let tweet = results[i]["full_text"].string {
+                    tweets.append(tweet)
+                }
+            }
+            
         }) { (error) in
             print(error)
         }
