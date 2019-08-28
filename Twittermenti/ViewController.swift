5b12d30c9db47b5cc7a947e559c79dfc185ce7ea
hamzahs34 <hamzah_shahid@hotmail.com>
2019-08-28 00:38:07 -0400

    Add Swifter and generated ML model to project

---
+++ Twittermenti/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Twittermenti
+//
+//  Created by Hamzah Shahid on 2019-08-27.
+//  Copyright © 2019 Hamzah Shahid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sentimentLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //ss
+    }
+
+    @IBAction func predictPressed(_ sender: Any) {
+    
+    
+    }
+    
+}
+
